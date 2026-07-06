b4b8472edfcd8aa13e8ba8155191749f35ad40b3
Hamza Suleiman Al Adawi <88661827+Hamza-Al-Adawi@users.noreply.github.com>
2026-07-07 01:22:37 +0400

    Revert "Revert "P3""

---
+++ Task5/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your raw score: ");
+            double rawScore = Convert.ToDouble(Console.ReadLine());
+
+            double roundedscore = Math.Round(rawScore);
+
+            if (roundedscore < 50 )
+            {
+                Console.WriteLine("Your grade is F");
+                Console.WriteLine("You Failed!");
+            }
+            else if (roundedscore >= 50 && roundedscore <= 69)
+            {
+                Console.WriteLine("Your grade is D");
+                Console.WriteLine("You Passed!");
+            }
+            else if (roundedscore >= 70 && roundedscore <= 79)
+            {
+                Console.WriteLine("Your grade is C");
+                Console.WriteLine("You Passed!");
+            }
+            else if (roundedscore >= 80 && roundedscore <= 89)
+            {
+                Console.WriteLine("Your grade is B");
+                Console.WriteLine("You Passed!");
+            }
+            else if (roundedscore >= 90 && roundedscore <= 100)
+            {
+                Console.WriteLine("Your grade is A");
+                Console.WriteLine("You Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid score entered.");
+            }
+        }
+    }
+}
---
+++ Task7/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Please enter same name: ");
+            string name2 = Console.ReadLine();
+
+            string n1 = name.Trim();
+            string n2 = name2.Trim();
+
+            bool nlnu = name.ToUpper() == name2.ToUpper();
+            bool nlnu2 = name.ToLower() == name2.ToLower();
+
+            if (name == name2 || n1 == n2 || nlnu || nlnu2)
+            {
+                Console.WriteLine(" Match ");
+            }
+            else
+            {
+                Console.WriteLine(" Not Match ");
+            }
+
+            Console.WriteLine("---------------------------------------");
+            Console.Write("do you want to open program again (y/n)? -- ");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("---------------------------------------");
+            string answer = Console.ReadLine();
+
+            if (answer == "y")
+            {
+                Console.WriteLine("Ok !");
+                Console.WriteLine("--");
+                Console.WriteLine("--");
+                Main(args);
+            }
+            else if (answer == "n")
+            {
+                Console.Write("Thanks For Using This Program !");
+                Console.WriteLine("--");
+                Console.WriteLine("--");
+            }
+            else
+            {
+                Console.Write("Invalid Input !");
+                Console.WriteLine("--");
+                Console.WriteLine("--");
+            }
+        }
+    }
+}
