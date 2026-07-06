6b850db83d08c027f383ed1dd2eba1fd6e41aba5
hamza <adawihsa@gmail.com>
2026-07-06 22:02:39 +0400

    Task 4 - Completed Subscription end date

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thanks For Subscription! ");
+
+            DateTime alyoom = DateTime.Today;
+            DateTime nextMonth = alyoom.AddDays(30);
+
+            Console.WriteLine("Your subscription will expire on: " + nextMonth );
+        }
+    }
+}
